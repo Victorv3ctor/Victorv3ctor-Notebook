3757325d493ed1bb0228fb30a73f34f77067a5a8
Wiktor Basta <mvpwiktor@MacBook-Pro-Wiktor.local>
2026-01-19 19:25:45 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from ui import Menu
+
+menu = Menu()
+try:
+    menu.run()
+except KeyboardInterrupt:
+    print(f'\nProgram zakonczony dzialanem spoza MENU')
+
+
+
+
+
+
+
+
+
+
---
+++ storage.py
@@ -0,0 +1,23 @@
+import csv
+
+from service import Notebook, Note
+
+class NotebookStorage:
+    @staticmethod
+    def from_file(filename):
+        notebook = Notebook()
+        with open(filename) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                note = Note(row['id'], row['title'], row['description'], row['date'], row['tags'])
+                notebook.add(note)
+        return notebook
+            #notebook storage
+
+    @staticmethod
+    def to_file(notebook, filename):
+        with open(filename, 'w') as file:
+            writer = csv.DictWriter(file,fieldnames = ['id', 'title', 'description', 'date', 'tags'])
+            writer.writeheader()
+            for note in notebook.notes:
+                writer.writerow(note.dict())
